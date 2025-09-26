14c3ec56476e0f65f9b2df932fe8aaa9339b8122
Sarp Erdoğan <sarpindcsi@gmail.com>
2025-09-26 13:00:11 +0300

    Bug Fix Commit

--- App.tsx
+++ App.tsx
@@ -3,46 +3,46 @@ import { View, Text, Button } from 'react-native';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import { store, RootState, AppDispatch } from './src/store';
 import { reset, connected } from './src/store/counterSlice';
-const count = useSelector((state: RootState) => state.counter.value);
-const dispatch = useDispatch<AppDispatch>();
-const ConnectButton = ():any => 
+const MainPageButtons = () => 
 {
-  return (
-    <Button
-    title = "Cihaza Bağlan"
-    onPress = {()=>{dispatch(reset)}}></Button>
-  );
-}
-const StartButton = ():any => 
-{
-  return (
-    <Button
-    title = "Cihazı Başlat"></Button>
-  );
-}
-const StopButton = ():any => 
-{
-  return (
-    <Button
-    title = "Cihazı Durdur"></Button>
-  );
-}
-const PhotoButton = ():any => 
-{
-  return (
-    <Button
-    title = "Fotoğraf Çek"></Button>
-  );
-}
-const SeeRecordsButton = ():any => 
-{
-  return(
-    <Button
-    title = "Kayıtları İncele"></Button>
-  );
-}
-const Main = () =>
-{
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch<AppDispatch>();
+  const ConnectButton = ():any => 
+  {
+    return (
+      <Button
+      title = "Cihaza Bağlan"
+      onPress = {()=>{dispatch(reset())}}></Button>
+    );
+  }
+  const StartButton = ():any => 
+  {
+    return (
+      <Button
+      title = "Cihazı Başlat"></Button>
+    );
+  }
+  const StopButton = ():any => 
+  {
+    return (
+      <Button
+      title = "Cihazı Durdur"></Button>
+    );
+  }
+  const PhotoButton = ():any => 
+  {
+    return (
+      <Button
+      title = "Fotoğraf Çek"></Button>
+    );
+  }
+  const SeeRecordsButton = ():any => 
+  {
+    return(
+      <Button
+      title = "Kayıtları İncele"></Button>
+    );
+  }
   return (
     <View>
       <ConnectButton/>
@@ -53,11 +53,12 @@ const Main = () =>
     </View>
   );
 }
+
 export default function App() 
 {
   return (
     <Provider store={store}>
-      <Main />
+      <MainPageButtons />
     </Provider>
   );
 }
